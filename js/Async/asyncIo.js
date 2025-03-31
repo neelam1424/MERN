447e1cb4e12582c32ef26a28c9857575343b7075
neelam1424 <neelammore1404@gmail.com>
2025-03-31 16:00:28 +0530

    asynchronous I/O

---
+++ js/Async/asyncIo.js
@@ -0,0 +1,14 @@
+const fs=require("fs")
+
+
+fs.readFile('a.txt',"utf-8",function(err,contents){
+    console.log(contents)
+})
+
+fs.readFile("b.txt",'utf-8',function(err,contents){
+    console.log(contents)
+})
+
+fs.readFile("c.txt",'utf-8',function(err,contents){
+    console.log(contents)
+})
