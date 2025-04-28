c2f83d27114361ab33b394cd178b58724f42b711
neelam1424 <neelammore1404@gmail.com>
2025-04-28 20:03:06 +0530

    week 6 authentication

---
+++ Authentication/index.js
@@ -0,0 +1,85 @@
+const express=require("express");
+const app=express();
+
+app.use(express.json());
+const users=[];
+
+function generateToken(){
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        // use a simple function here
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+    return token;
+}
+
+
+app.post("/signup",(req,res)=>{
+
+    const username=req.body.username;
+    const password=req.body.password;
+
+    users.push({
+        username,
+        password
+    })
+
+    res.send({
+        message:"You have signed up"
+    })
+
+});
+
+app.post("/signin",(req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+//generate token only when username and password is correct
+    const user=users.find(user=>user.username ===username && user.password===password);
+
+    if(user){
+        const token =generateToken();
+        user.token=token;
+        res.send({
+            token
+        })
+        console.log(users);
+    }else{
+        res.status(403).send({
+            message:"Invalid username or password"
+        })
+    }
+});
+
+ app.get("/me",(req,res)=>{
+    const token=req.headers.token;
+    let foundUser=null;
+
+    for(let i=0;i<users.length;i++){
+        if(users[i].token==token){
+            foundUser=users[i];
+        }
+    }
+    if(foundUser){
+        res.json({
+            username:foundUser.username,
+            password: foundUser.password
+        })
+    }else{
+        res.json({
+            message:"token invalid"
+        })
+    }
+    // let user=user.find(user=>user.token===token);
+    // if (user){
+    //     res.send({
+    //         username:user.username
+    //     })
+    // }else{
+    //     res.status(400).send({
+    //         message:"Unauthorized"
+    //     })
+    // }
+ })
+app.listen(3000);
